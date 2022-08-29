0cdad6e942febaca06705810163cf122c312eb48
Roderick Fayson <rgfayson2@gmail.com>
2022-08-29 09:45:06 -0400

    first-push

---
+++ functions/index.js
@@ -0,0 +1,18 @@
+import functions from "firebase-functions"
+import express from "express"
+import cors from "cors"
+import { getTasks, createTask, updateTask, deleteTask } from "src.task.js"
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/tasks', getTasks)
+app.post('/tasks', createTask)
+app.patch('/tasks/:taskId', updateTask)
+app.delete('/tasks/:taskId', deleteTask)
+
+
+export const api = functions.https.onRequest(app);
