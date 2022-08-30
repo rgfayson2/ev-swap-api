1f0f8eaf17e2da5d946687a6cf88b14d71d0121c
Roderick Fayson <rgfayson2@gmail.com>
2022-08-30 16:21:50 -0400

    last push of the day

--- functions/src/Cars.js
+++ functions/src/Cars.js
@@ -4,23 +4,23 @@ export async function getCar(req, res) {
     const db = dbConnect()
     const collection = await db.collection('cars').get()
     .catch((err) => req.status(500).send(err))
-    const car = collection.docs.map((doc) => {
+    const cars = collection.docs.map((doc) => {
         let car = doc.data()
         car.id = doc.id
         return car
     })
-    res.send(car)
+    res.send(cars)
 }
 
 export async function createCar(req, res) {
     const newCar = req.body
-    if (!newCar || !newCar.car) {
+    if (!newCar || !newCar.model) {
         res.status(400).send({success: false, message: "Invalid request"})
         return
     }
     const db = dbConnect()
     await db
-    .collection
+    .collection('cars')
     .add(newCar)
     .catch((err) => res.status(500).send(err))
     res.status(201)
