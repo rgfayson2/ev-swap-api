73e65cc370c86889e4f8e134348db5e46696398b
Roderick Fayson <rgfayson2@gmail.com>
2022-09-07 17:21:51 -0400

    changed the port

--- functions/src/Cars.js
+++ functions/src/Cars.js
@@ -1,32 +1,29 @@
-import dbConnect from "./dbConnect.js";
+import dbConnect from "./dbConnect.js"
 
 export async function getCar(req, res) {
-    const db = dbConnect()
-    const collection = await db.collection('cars').get()
+  const db = dbConnect()
+  const collection = await db
+    .collection("cars")
+    .get()
     .catch((err) => req.status(500).send(err))
-    const cars = collection.docs.map((doc) => {
-        let car = doc.data()
-        car.id = doc.id
-        return car
-    })
-    res.send(cars)
+  const cars = collection.docs.map((doc) => {
+    let car = doc.data()
+    car.id = doc.id
+    return car
+  })
+  res.send(cars)
 }
 
 export async function createCar(req, res) {
-    const newCar = req.body
-    if (!newCar || !newCar.model) {
-        res.status(400).send({success: false, message: "Invalid request"})
-        return
-    }
-    const db = dbConnect()
-    await db
-    .collection('cars')
+  const newCar = req.body
+  //   if (newCar) {
+  //     res.status(400).send({ success: false, message: "Invalid request" })
+  //     return
+  //   }
+  const db = dbConnect()
+  await db
+    .collection("cars")
     .add(newCar)
+    .then((doc) => res.status(201).send(doc.id))
     .catch((err) => res.status(500).send(err))
-    res.status(201)
-    getCar(req, res)
 }
-
-
-
-
