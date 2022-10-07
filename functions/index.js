d3dee58b514354345e3aff9f125ee270bc797504
Roderick Fayson <rgfayson2@gmail.com>
2022-10-07 13:02:47 -0400

    sign up not working

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 import functions from "firebase-functions"
 import express from "express"
 import cors from "cors"
-import { getCar, createCar, } from "./src/Cars.js"
+import { getCar, createCar,} from "./src/Cars.js"
+import { getUser, createUser,} from "./src/User.js"
 
 
 const app = express()
@@ -11,6 +12,7 @@ app.use(express.json())
 
 app.get('/cars', getCar)
 app.post('/cars', createCar)
-app.post('/user', createUser)
+app.get('/users', getUser)
+app.post('/users', createUser)
 
 export const api = functions.https.onRequest(app);
